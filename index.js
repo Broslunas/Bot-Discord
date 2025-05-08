150b4c8bc402ffa2b3107bac210258663d1f36e0
Broslunas <pablo@broslunas.com>
2025-05-08 18:39:20 +0100

    Remove ping command and update command registration to use applicationCommands

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v10");
 
 const commands = [
-  { name: "ping", description: "Responde con pong y muestra botones" },
   {
     name: "mail",
     description: "Envía un modmail al owner",
@@ -63,13 +62,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
   try {
     console.log("Registrando comandos…");
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
-      { body: commands }
-    );
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
     console.log("Comandos registrados correctamente");
   } catch (error) {
     console.error(error);
